2bf10e033fc0f34de7400a246c28267277566697
Tim Lenertz <timlenertz@gmail.com>
2016-04-14 13:06:29 +0200

    tests

--- test/media_graph.cc
+++ test/media_graph.cc
@@ -12,14 +12,12 @@ TEST_CASE("media graph", "[media_graph]") {
 	media_graph graph;
 	auto shp = make_ndsize(320, 240);
 
-	std::vector<int> seq(2);
+	std::vector<int> seq(20);
 	for(int i = 0; i < seq.size(); ++i) seq[i] = i;	
 
 	set_debug_mode(debug_mode::file);
 
 	SECTION("source -> sink") {
-	MF_DEBUG("AA************************************************************************************");
-
 		auto& source = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
 		auto& sink = graph.add_sink<expected_frames_sink>(seq);
 		sink.input.connect(source.output);
@@ -30,8 +28,6 @@ TEST_CASE("media graph", "[media_graph]") {
 
 
 	SECTION("source --> passthrough --> sink") {
-	MF_DEBUG("BB************************************************************************************");
-
 		auto& source = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
 		auto& passthrough = graph.add_node<passthrough_node>(0, 0);
 		auto& sink = graph.add_sink<expected_frames_sink>(seq);
@@ -49,8 +45,6 @@ TEST_CASE("media graph", "[media_graph]") {
 
 
 		SECTION("source [-3]--> pass --> sink") {
-			MF_DEBUG("CC************************************************************************************");
-
 			auto& node = graph.add_node<passthrough_node>(3, 0);
 	
 			node.input.connect(source.output);
@@ -155,7 +149,7 @@ TEST_CASE("media graph", "[media_graph]") {
 			REQUIRE(sink.check());
 		}
 
-/*
+
 		SECTION("source [-3,+3]--> sink") {
 			auto& node = graph.add_node<passthrough_node>(3, 3);
 	
@@ -164,61 +158,146 @@ TEST_CASE("media graph", "[media_graph]") {
 			graph.setup();
 			
 			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 0);
-				REQUIRE(in.view() == make_frame(shp, 0));
+				switch(self.current_time()) {
+				case 0:
+					REQUIRE(in.view() == make_frame(shp, 0));
+					REQUIRE(in.full_view_center() == 0);
+					REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3 }));
+					break;
+	
+				case 1:
+					REQUIRE(in.view() == make_frame(shp, 1));
+					REQUIRE(in.full_view_center() == 1);
+					REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4 }));
+					break;
+	
+				case 2:
+					REQUIRE(in.view() == make_frame(shp, 2));
+					REQUIRE(in.full_view_center() == 2);
+					REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4, 5 }));
+					break;
+		
+				case 3:
+					REQUIRE(in.view() == make_frame(shp, 3));
+					REQUIRE(in.full_view_center() == 3);
+					REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4, 5 }));
+					break;
+					
+				case 4:
+					REQUIRE(in.view() == make_frame(shp, 4));
+					REQUIRE(in.full_view_center() == 3);
+					REQUIRE(compare_frames(shp, in.full_view(), { 1, 2, 3, 4, 5 }));
+					break;
 				
-				REQUIRE(in.full_view_center() == 0);
-				REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3 }));
+				case 5:
+					REQUIRE(in.view() == make_frame(shp, 5));
+					REQUIRE(in.full_view_center() == 3);
+					REQUIRE(compare_frames(shp, in.full_view(), { 2, 3, 4, 5 }));
+					break;
+				}
 			});
-			graph.run_until(0);
-			
-			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 1);
-				REQUIRE(in.view() == make_frame(shp, 1));
-				
-				REQUIRE(in.full_view_center() == 1);
-				REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4 }));
-			});
-			graph.run_until(1);
-			
-			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 2);
-				REQUIRE(in.view() == make_frame(shp, 2));
-				
-				REQUIRE(in.full_view_center() == 2);
-				REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4, 5 }));
-			});
-			graph.run_until(2);
-			
-			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 3);
-				REQUIRE(in.view() == make_frame(shp, 3));
-				
-				REQUIRE(in.full_view_center() == 3);
-				REQUIRE(compare_frames(shp, in.full_view(), { 0, 1, 2, 3, 4, 5 }));
-			});
-			graph.run_until(3);
-			
-			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 4);
-				REQUIRE(in.view() == make_frame(shp, 4));
-				
-				REQUIRE(in.full_view_center() == 3);
-				REQUIRE(compare_frames(shp, in.full_view(), { 1, 2, 3, 4, 5 }));
-			});
-			graph.run_until(4);
-			
-			node.set_callback([&](passthrough_node& self, auto& in, auto& out) {
-				REQUIRE(self.current_time() == 5);
-				REQUIRE(in.view() == make_frame(shp, 5));
-				
-				REQUIRE(in.full_view_center() == 3);
-				REQUIRE(compare_frames(shp, in.full_view(), { 2, 3, 4, 5 }));
-			});
-			graph.run_until(5);	
-			
-			REQUIRE(sink.got_expected_frames());
+
+			graph.run();
+			REQUIRE(sink.check());
 		}
-		*/
 	}
+	
+
+	SECTION("source1 --> [+3]passthrough1 --> sink") {
+		auto& source1 = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
+		auto& passthrough1 = graph.add_node<passthrough_node>(0, 3);
+		auto& sink = graph.add_sink<expected_frames_sink>(seq);
+
+		passthrough1.input.connect(source1.output);
+		sink.input.connect(passthrough1.output);
+		
+		graph.setup();
+
+		REQUIRE(sink.offset() == 0);
+		REQUIRE(passthrough1.offset() == 0);
+		REQUIRE(source1.offset() == 3);
+		
+		REQUIRE(passthrough1.output.required_buffer_duration() == 1);
+		REQUIRE(source1.output.required_buffer_duration() == 4);
+
+		graph.run();
+		
+		REQUIRE(graph.current_time() == seq.size()-1);
+		REQUIRE(sink.check());
+	}
+	
+	
+	SECTION("source1 --> [-3]passthrough1 --> sink") {
+		auto& source1 = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
+		auto& passthrough1 = graph.add_node<passthrough_node>(3, 0);
+		auto& sink = graph.add_sink<expected_frames_sink>(seq);
+
+		passthrough1.input.connect(source1.output);
+		sink.input.connect(passthrough1.output);
+		
+		graph.setup();
+
+		REQUIRE(sink.offset() == 0);
+		REQUIRE(passthrough1.offset() == 0);
+		REQUIRE(source1.offset() == 0);
+		
+		REQUIRE(passthrough1.output.required_buffer_duration() == 1);
+		REQUIRE(source1.output.required_buffer_duration() == 4);
+
+		graph.run();
+				
+		REQUIRE(graph.current_time() == seq.size()-1);
+		REQUIRE(sink.check());
+	}
+	
+	SECTION("source1 --> [-3,+3]passthrough1 --> sink") {
+		auto& source1 = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
+		auto& passthrough1 = graph.add_node<passthrough_node>(3, 3);
+		auto& sink = graph.add_sink<expected_frames_sink>(seq);
+
+		passthrough1.input.connect(source1.output);
+		sink.input.connect(passthrough1.output);
+		
+		graph.setup();
+
+		REQUIRE(sink.offset() == 0);
+		REQUIRE(passthrough1.offset() == 0);
+		REQUIRE(source1.offset() == 3);
+		
+		REQUIRE(passthrough1.output.required_buffer_duration() == 1);
+		REQUIRE(source1.output.required_buffer_duration() == 7);
+
+		graph.run();
+				
+		REQUIRE(graph.current_time() == seq.size()-1);
+		REQUIRE(sink.check());
+	}
+
+	SECTION("source1 --> [-3,+1]passthrough1 --> [-2,+2]passthrough2 --> sink") {
+		auto& source1 = graph.add_node<sequence_frame_source>(seq.size()-1, shp, true);
+		auto& passthrough1 = graph.add_node<passthrough_node>(3, 1);
+		auto& passthrough2 = graph.add_node<passthrough_node>(2, 2);
+		auto& sink = graph.add_sink<expected_frames_sink>(seq);
+
+		passthrough1.input.connect(source1.output);
+		passthrough2.input.connect(passthrough1.output);
+		sink.input.connect(passthrough2.output);
+		
+		graph.setup();
+
+		REQUIRE(sink.offset() == 0);
+		REQUIRE(passthrough2.offset() == 0);
+		REQUIRE(passthrough1.offset() == 2);
+		REQUIRE(source1.offset() == 3);
+		
+		REQUIRE(passthrough2.output.required_buffer_duration() == 1);
+		REQUIRE(passthrough1.output.required_buffer_duration() == 5);
+		REQUIRE(source1.output.required_buffer_duration() == 5);
+
+		graph.run();
+		
+		REQUIRE(graph.current_time() == seq.size()-1);
+		REQUIRE(sink.check());
+	}
+
 }
